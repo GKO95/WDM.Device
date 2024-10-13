2f744b245ad77c793f35f45be84bee763851d8f5
Gihwan Ko <44629526+GKO95@users.noreply.github.com>
2024-10-13 14:32:49 +0900

    Add project files.

---
+++ Win32Application/Win32Application.c
@@ -0,0 +1,15 @@
+#include "Win32Application.h"
+
+#pragma warning(disable: 28251)
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+
+	_tprintf(_T("Hello, World!\n"));
+
+	return 0;
+}
+#pragma warning(default: 28251)
